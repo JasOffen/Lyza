d02ac071bf50a3fd1c9d5ab9777d9d926f1c69ba
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-01-25 13:44:54 -0700

    displaying multiple photos on screen

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,6 +3,12 @@ import {render, cleanup, getByTestId} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup)
 
 describe('Nav component', () =>{
@@ -32,4 +38,14 @@ describe('links are visible', () =>{
         expect(getByTestId('link')).toHaveTextContent('Oh Snap');
         expect(getByTestId('about')).toHaveTextContent('About me');
     });
+})
+
+describe('rendering the nav', () =>{
+    it('renders', () => {
+        render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+        />);
+      })
 })
