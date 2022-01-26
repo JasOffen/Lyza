745853cbe55876f1b2918f1fb4b59248717490bc
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-01-25 17:18:55 -0700

    created Modal component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,10 @@ const categories = [
   ]
   const mockCurrentCategory = jest.fn();
   const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
 
 afterEach(cleanup)
 
@@ -46,6 +50,9 @@ describe('rendering the nav', () =>{
           categories={categories}
           setCurrentCategory={mockSetCurrentCategory}
           currentCategory={mockCurrentCategory}
+          contactSelected={mockContactSelected}
+          setContactSelected={mockSetContactSelected}
         />);
       })
 })
+
