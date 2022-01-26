62bbd308e09d0fb295537a4bd837bc8d7f6699d2
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-01-25 18:07:29 -0700

    Added togglable modals for images

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 
 
-function Modal({currentPhoto}) {
-    const {name, category, description, index} = currentPhoto;
-  
+const Modal = ({ onClose, currentPhoto }) => {
+    const { name, category, description, index } = currentPhoto;
+
+
     return (
-      <div className="modalBackdrop">
-        <div className="modalContainer">
-          <h3 className="modalTitle">{name}</h3>
-          <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
-          <p>{description}</p>
-                <button type="button">
-                    Close this modal
-                </button>
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">{name}</h3>
+                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+                <p>{description}</p> 
+                <button onClick={onClose} type="button">Close this modal</button> 
             </div>
         </div>
     );
